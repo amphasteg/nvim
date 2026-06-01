9421761c667b8c8c0d203f246ccb751812cc0ce2
Zach Nichols <git@zachdnichols.com>
2026-06-01 13:41:51 -0500

    Autocomplete and snippets

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,6 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
-vim.keymap.set("n", "<C-n>", ':Neotree filesystem reveal left<CR>')
+vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
 
 --LSP
-vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
