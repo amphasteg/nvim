2be05e515bfa83beb9dd3172e13da28ce11c7378
Zach Nichols <git@zachdnichols.com>
2026-06-03 10:17:18 -0500

    Add C# to lsps

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,33 +1,36 @@
 local lsps = {
-  "lua_ls",
-  "rust_analyzer",
-  "codebook",
-  "clangd",
-  "stylua",
-  "tsserver",
-  "html",
-  "eslint",
-  "tsserver", 
+	"lua_ls",
+	"rust_analyzer",
+	"codebook",
+	"clangd",
+	"stylua",
+	"tsserver",
+	"html",
+	"eslint",
+	"tsserver",
+	"roslyn-language-server",
 }
 
 return {
-  "mason-org/mason-lspconfig.nvim",
-  lazy = false,
-  config = function()
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	"mason-org/mason-lspconfig.nvim",
+	lazy = false,
+	config = function()
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    for i,v in ipairs(lsps) do
-      vim.lsp.config(v, { capabilities = capabilities })
-      vim.lsp.enable(v)
-      vim.diagnostic.config({ virtual_text = true, update_in_insert = true })
-    end
-  end,
+		for i, v in ipairs(lsps) do
+			vim.lsp.config(v, { capabilities = capabilities })
+			vim.lsp.enable(v)
+			vim.diagnostic.config({ virtual_text = true, update_in_insert = true })
+		end
 
-  dependencies = {
-    { "mason-org/mason.nvim",  opts = {} },
-    { "neovim/nvim-lspconfig", lazy = false },
-  },
-  opts = {
-    ensure_installed = { lsps },
-  },
+		vim.lsp.config("codebook", {})
+	end,
+
+	dependencies = {
+		{ "mason-org/mason.nvim", opts = {} },
+		{ "neovim/nvim-lspconfig", lazy = false },
+	},
+	opts = {
+		ensure_installed = { lsps },
+	},
 }
