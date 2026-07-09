c1aaa423fff9a52728ed91bf375c586e03b9892d
Zach Nichols <git@zachdnichols.com>
2026-07-08 21:38:25 -0500

    Change option for comments

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,6 @@ vim.opt.expandtab = true
 vim.opt.tabstop=2
 vim.opt.softtabstop=2
 vim.opt.shiftwidth=2
+
+vim.cmd('autocmd BufEnter * set formatoptions-=cro')
+vim.cmd('autocmd BufEnter * setlocal formatoptions-=cro')
