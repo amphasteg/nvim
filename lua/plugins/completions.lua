3fdfe92fe09b119bdd5047bd3ee9a818c51a4fcc
Zach Nichols <zach@zachdnichols.com>
2026-08-06 21:45:58 -0500

    Fix completions inserting twice, tab to next arg

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -13,14 +13,14 @@ return {
 		"hrsh7th/nvim-cmp",
 		config = function()
 			local cmp = require("cmp")
+      local luasnip = require("luasnip")
 			require("luasnip.loaders.from_vscode").lazy_load()
 			cmp.setup({
-
 				snippet = {
 					-- REQUIRED - you must specify a snippet engine
 					expand = function(args)
-						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-						vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+						luasnip.lsp_expand(args.body) -- For `luasnip` users.
+						--vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
 					end,
 				},
 				window = {
@@ -33,7 +33,16 @@ return {
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
-				}),
+           ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end),
+          }),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" }, -- For luasnip users.
