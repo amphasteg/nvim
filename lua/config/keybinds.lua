9aaad086ee4ab5d949aad26e55bad86d25cf0524
Zach Nichols <github@zachdnichols.com>
2026-08-21 21:33:24 -0500

    Switch from none-ls to conform and nvim-lint

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -3,15 +3,22 @@ vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
 vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
 
 --LSP
+local conform = require("conform")
 vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+vim.keymap.set("n", "<leader>gf", function()
+	conform.format({
+		lsp_fallback = true,
+		async = false,
+		timeout_ms = 500,
+	})
+end, {})
 vim.keymap.set("n", "]g", vim.diagnostic.goto_next)
 vim.keymap.set("n", "[g", vim.diagnostic.goto_prev)
 
--- Debugging 
+-- Debugging
 local dap = require("dap")
-vim.keymap.set('n', '<leader>dt', dap.toggle_breakpoint, {})
-vim.keymap.set('n', '<leader>dc', dap.continue, {})
+vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})
+vim.keymap.set("n", "<leader>dc", dap.continue, {})
 
 -- Default key-binds
 -- SHIFT+K = view function documentation
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,3 +7,14 @@ vim.opt.shiftwidth=2
 
 vim.cmd('autocmd BufEnter * set formatoptions-=cro')
 vim.cmd('autocmd BufEnter * setlocal formatoptions-=cro')
+
+vim.api.nvim_create_user_command("LintInfo", function()
+  local filetype = vim.bo.filetype
+  local linters = require("lint").linters_by_ft[filetype]
+
+  if linters then
+    print("Linters for " .. filetype .. ": " .. table.concat(linters, ", "))
+  else
+    print("No linters configured for filetype: " .. filetype)
+  end
+end, {})
