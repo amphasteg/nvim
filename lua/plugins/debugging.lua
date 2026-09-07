9e744525a21ae426a9fb2bb0d672a7a0e0c2fddc
Zach Nichols <github@zachdnichols.com>
2026-09-06 22:57:42 -0500

    More dap keybinds

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,46 +1,47 @@
 return {
-  "mfussenegger/nvim-dap",
-  lazy = false,
-  dependencies = {
-    "rcarriga/nvim-dap-ui",
-    "theHamsta/nvim-dap-virtual-text",
-    "nvim-neotest/nvim-nio",
-  },
-  config = function()
-    local dap, dapui = require("dap"), require("dapui")
-    
-    dapui.setup()
+	"mfussenegger/nvim-dap",
+	lazy = false,
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"theHamsta/nvim-dap-virtual-text",
+		"nvim-neotest/nvim-nio",
+	},
+	config = function()
+		local dap, dapui = require("dap"), require("dapui")
 
-    dap.adapters.codelldb = {
-      type = "executable",
-      command = "codelldb",
-      detached = false,
-    }
+		dapui.setup()
+    require("nvim-dap-virtual-text").setup()
 
-    dap.configurations.c = {
-      {
-        name = "Launch file",
-        type = "codelldb",
-        request = "launch",
-        program = function()
-          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-        end,
-        cwd = '${workspaceFolder}',
-        stopOnEntry = false,
-      },
-    }
+		dap.adapters.codelldb = {
+			type = "executable",
+			command = "codelldb",
+			detached = false,
+		}
 
-    dap.listeners.before.attach.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.launch.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated.dapui_config = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited.dapui_config = function()
-      dapui.close()
-    end
-end,
+		dap.configurations.c = {
+			{
+				name = "Launch file",
+				type = "codelldb",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+				stopOnEntry = false,
+			},
+		}
+
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+	end,
 }
---
+++ lua/plugins/overseer.lua
@@ -0,0 +1,13 @@
+return {
+	{
+		"stevearc/overseer.nvim",
+		---@module 'overseer'
+		---@type overseer.SetupOpts
+		opts = {},
+    config = function()
+      require("overseer").setup()
+    end,
+
+      
+	},
+}
