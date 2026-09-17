be3c25b4f6d20de59c4ad1b29a4d90c60589e246
Zach Nichols <github@zachdnichols.com>
2026-09-16 22:07:43 -0500

    Color scheme changes

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -5,10 +5,17 @@ return {
 		lazy = false,
 		priority = 1000,
 		config = function()
-			vim.cmd([[colorscheme catppuccin-nvim]])
       require("catppuccin").setup({
-        auto_integrations = true
+        auto_integrations = true,
+        color_overrides = {
+          all = {
+            mantle = "#171717",
+            crust = "#171717",
+            base = "#1A1A1A",
+          },
+        }
       })
+			vim.cmd([[colorscheme catppuccin-nvim]])
 		end,
 	},
 }
